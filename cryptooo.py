93beab78ec5d50e46d9ec5f375d73ca28cf90b92
Shreyank-H <121376693+Shreyank-H@users.noreply.github.com>
2025-02-21 18:41:40 +0530

    Update cryptooo.py

--- cryptooo.py
+++ cryptooo.py
@@ -5,7 +5,7 @@ from telegram import Bot
 import os
 
 # Binance Announcements URL
-BINANCE_ANNOUNCEMENT_URL = "https://www.binance.com/en/support/announcement"
+BINANCE_ANNOUNCEMENT_URL = "https://www.binance.com/en/support/announcement/list/48"
 
 # Telegram Bot Credentials
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
